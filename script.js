5c5eeef461ba7711180badeaf6336063da534148
colmo0912 <colmo0912@gmail.com>
2021-04-17 19:03:27 +0200

    inserisco ultime modifiche

--- script.js
+++ script.js
@@ -37,21 +37,76 @@
 //     document.write("Il prezzo del tuo biglietto è " + prezzoBiglietto +" euro" )
 // }
 
+
+
+// -----------------------------------------------
+
+
 var formUtente = document.getElementById("formUtente");
-
 formUtente.addEventListener("submit",function(event){
-    event.preventDefault();
-    // console.log("ciao")
+    console.log("submit del form")
+    event.preventDefault()
+
     var form = event.currentTarget;
-    var formElements = form.elements
+    var formElementi= form.elements
 
+    var prezzoAlKm = 0.21;
+    var nome=formElementi.nome.value
+    var kmViaggio=formElementi.kmViaggio.value
+    var selezionaEta =formElementi.selezionaEta.value
+    var nomePasseggero = document.getElementById("nomePasseggero");
+    nomePasseggero += nomePasseggero.innerHTML = "NOME PASSEGGERO " + formElementi.nome.value;
+    var costoBiglietto = document.getElementById("costoBiglietto");
+    console.log(prezzoAlKm)
+    console.log(kmViaggio)
+    var costoBigliettoTemp = prezzoAlKm * kmViaggio
+    var offerta = document.getElementById("offerta")
+    var offertaBiglietto = "BIGLIETTO STANDARD"
+
+
+    if (selezionaEta === "over"){
+
+        costoBigliettoTemp -= ((costoBigliettoTemp*60)/100)
+        offertaBiglietto = "SCONTO OVER 65"
+
+    }else if(selezionaEta === "minorenne"){
+        costoBigliettoTemp -= ((costoBigliettoTemp*20)/100)
+        offertaBiglietto = "SCONTO UNDER 18"
+    }
+
+    offerta.innerHTML = offertaBiglietto
+
+    costoBiglietto.innerHTML= costoBigliettoTemp.toFixed(2) + " euro";
+
+    var carrozza = document.getElementById("carrozza")
+    carrozza.innerHTML = (Math.floor(Math.random() * 10) + 1); 
+
+    var codiceCP = document.getElementById("codiceCP")
+    codiceCP.innerHTML = (Math.floor(Math.random() * ((99999 - 90000) +1) + 90000)); 
+
+    document.getElementById("biglietto").classList.remove("hiddenTicket")
+
+}
+
+
+)
+formUtente.addEventListener("reset",function(event){
+
+    document.getElementById("nomePasseggero").innerHTML=""
+    document.getElementById("costoBiglietto").innerHTML=""
+    document.getElementById("carrozza").innerHTML=""
+    document.getElementById("codiceCP").innerHTML=""
+    document.getElementById("offerta").innerHTML=""
     
-    var nome =formElements.nome.value
-    var kmViaggio =formElements.kmViaggio.value
-    var eta =formElements.eta.value
+}
+)
 
-    console.log(nome + kmViaggio)
-})
+
+
+
+
+
+
 
 
 
